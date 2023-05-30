17034f86493fabff3f743934dff02bbb0e07b5dd
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2023-05-30 15:36:27 +0200

    Fix db external sh

--- install/install_codeface_ubuntu_22.04_external_db.sh
+++ install/install_codeface_ubuntu_22.04_external_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parameters: [external_db_1.conf] [external_db_2.conf] ...
-
+# where the conf paths are relative to the codeface folder, *not* the install folder
 cd ..
 ./install/install_repositories.sh
 ./install/install_common_external_db.sh
