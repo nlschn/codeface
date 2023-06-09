ce62f3a129f46bd6ad5b2b34f4c0fc293c28b5bc
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2023-06-09 08:44:00 +0200

    Add check to installation whether R repository is already installed

--- install/install_repositories.sh
+++ install/install_repositories.sh
@@ -6,10 +6,17 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install software-properties-com
 echo "Adding R cran repositories"
 version=`lsb_release -r | awk '{ print $2;}'`
 
+REPO="deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/"
+APTFILE="/etc/apt/sources.list"
 # This is for Ubuntu 22.04
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list
+if grep -q "$REPO" "$APTFILE" ; then
+    echo "R repo is already in sources.list"
+else
+    echo "Add R repo to sources.list"
+    echo "$REPO" | sudo tee -a "$APTFILE"
+    wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+fi
 
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo apt update
 
 sudo apt-get update -qq
