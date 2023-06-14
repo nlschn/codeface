04293e10107d968ea3d97f5dd49c4e31245461ed
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2023-06-14 12:02:52 +0200

    Debug

--- codeface/util.py
+++ codeface/util.py
@@ -447,7 +447,7 @@ def parse_iso_git_date(date_string):
     # this way we don't need the above workaround, currently %z isn't
     # working as documented
     fmt = "%Y-%m-%d %H:%M:%S"
-    logging.info(date_string)
+    logging.error(date_string)
     parsed_date = datetime.strptime(date_string[:-6], fmt)
     parsed_date -= delta
     return parsed_date
