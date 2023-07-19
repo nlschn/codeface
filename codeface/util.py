b8ab02b4847ee284a1d4c1b512fe1d149faf1547
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2023-07-19 15:01:43 +0200

    Debug

--- codeface/util.py
+++ codeface/util.py
@@ -282,7 +282,7 @@ def execute_command(cmd, ignore_errors=False, direct_io=False, cwd=None, silent_
             if not(silent_errors):
                 log.error(msg)
             raise Exception(msg)
-    return stdout.decode("utf-8")
+    return stdout
 
 def _convert_dot_file(dotfile):
     '''
